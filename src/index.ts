59ff095a9c0fe4095b957bb4344059d323c174ba
thethiago27 <thethiago27@gmail.com>
2023-10-03 15:03:33 -0400

    feat: fix vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import { App } from './server/app'
 import 'dotenv/config'
+import awsLambdaFastify from '@fastify/aws-lambda'
 import { modelInit } from './models/song-predict'
 
 const app = new App()
+app.registerRoutes()
 
-;(async () => {
+modelInit()
+
+const proxy = awsLambdaFastify(app.server, {
+  callbackWaitsForEmptyEventLoop: false,
+})
+
+export const lambdaHandler = async (event: any, context: any): Promise<any> => {
+  await app.server.ready()
+  return proxy(event, context)
+}
+
+if (require.main === module) {
   try {
-    await modelInit()
-
-    await app.start()
-  } catch (e) {
-    console.error(e)
+    const serverPort = 5013
+    app.server.listen({ port: serverPort }, () => {
+      console.log(`Server is running on port ${serverPort}`)
+    })
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
   }
-})()
+}
